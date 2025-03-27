1e84a59acd42854b56cb01ecb22e8b9f05ac1e05
mateus.krauss@gmail.com <matheus.pimenta@sindec.pms.ad>
2025-03-27 10:15:23 -0300

    Adiciona React ao projeto para o view, também cria rotas

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/');
+// rotas despesas
+router.post('/adicionar-despesa');
+router.put('/editar-despesa');   
+router.delete('/deletar-despesa');
+
+// rotas receita
+router.post('/adicionar-receita');
+router.put('/editar-receita');
+router.delete('/deletar-receita');
+
+// roda despesas e receitas 
+router.get('/receitas-despesas');
 
 module.exports = router;
